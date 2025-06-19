405ce23b57d71ab94050b7a2d322b8dd1e7c1b14
Daniel Quintão <danielqferes@gmail.com>
2025-06-19 00:03:29 -0300

    feat: start binary tree implementation with insert function

--- data_structures/binary_tree/main.c
+++ data_structures/binary_tree/main.c
@@ -1,6 +1,52 @@
-// main.c
-// Projeto: binary_tree
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+typedef struct Node {
+    int value;
+    Node* left;
+    Node* right
+} Node;
+
+typedef struct {
+    Node* root;
+} Tree;
+
+void createTree(Tree* t) {
+    t->root = NULL;
+}
+
+void isEmpty(Tree* t) {
+    return t->root == NULL;
+}
+
+Node* createNewNode(int value) {
+    Node* newNode = malloc(sizeof(Node));
+    if (!newNode) {
+        puts("Error: memory allocation failed!");
+        exit(1);
+    }
+    newNode->value = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node* root, int value) {
+    if (root == NULL) {
+        Node* newNode = malloc(sizeof(Node));
+        if (!newNode) {
+            puts("Erro: memory allocation failed!");
+            exit(1);
+        }
+
+        newNode->value = value;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+
+    if (value < root->value) root->left = insert(root->left, value);
+    else root->right = insert(root->right, value);
+
+    return root;
 }
